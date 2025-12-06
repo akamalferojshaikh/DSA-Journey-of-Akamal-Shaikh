3974d9d7d1f1592c89e5aebab45c3277ad6118b2
akamalferojshaikh <akamalferojshaikh@gmail.com>
2025-12-07 00:17:29 +0530

    Added Inverted Pyramid logic using nested loops dsa_day3.cpp

--- day_3/dsa_day3.cpp
+++ day_3/dsa_day3.cpp
@@ -1,6 +1,7 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
+int main()
+{
     // *
     // **
     // ***
@@ -20,24 +21,23 @@ int main(){
     //     }
     //     cout<<endl;
     // }
-    
+
     //      *
     //     **
     //    ***
     //   ****
-    //  *****    
+    //  *****
 
-    for(int i=1;i<=5;i++){
-        for(int space = 5;space>i;space--){
-            cout<< " ";
-        }
-        for(int st=i;st>=1;st--){
-            cout<<"*";
-        }
-        cout<<endl;
-    }
+    // for(int i=1;i<=5;i++){
+    //     for(int space = 5;space>i;space--){
+    //         cout<<"*";
+    //     }
+    //     for(int st=i;st>=1;st--){
+    //         cout<< " ";
+    //     }
+    //     cout<<endl;
+    // }
 
-   
     // 1 2 3 4 5
     // 6 7 8 9 10
     // 11 12 13 14 15
@@ -52,10 +52,74 @@ int main(){
     //     }
     //     cout<<endl;
     //}
+
+    //     *
+    //    ***
+    //   *****
+    //  *******
+    // *********
+
+    for(int i=5; i>=1; i--){ //this loop is for 5 lines
+        //left wali space print karne
+        for(int L=5; L>=i; L--){
+            cout<< " ";
+        }
+        //star prinitng
+        for(int S=1; S<=(2*i-1);S++){
+            cout<<"*";
+        }
+        //right wali space print karne
+        for(int R=5; R>=i; R--){
+            cout<< " ";
+        }
+        cout<<endl;
+    }
     
 
 
+    // for (int space = 1; space <= 4; space++)
+    // {   cout << "-";     }
+    // for (int star = 1; star <= 1; star++)
+    // {   cout << "*";    }
+    // for (int space1 = 1; space1 <= 4; space1++)
+    // {   cout << "-";    }
+    // cout<<endl;
+    // for (int space = 1; space <= 3; space++)
+    // {   cout << "-";    }
+    // for (int star = 1; star <= 3; star++)
+    // {   cout << "*";    }
+    // for (int space1 = 1; space1 <= 3; space1++)
+    // {   cout << "-";    }
+    // cout<<endl;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // for(int i=1; i<=5; i++){
+    //     //space wala loop
+    //     // for(int space=4; space>=i;space--){
+    //     //     cout<< " ";
+    //     // }
+        
+    //     // for(int star=1; star<=25;star++){
+    //     //     cout<< "*";
+    //     // }
+        
+    //     // for(int space=4; space>=i;space--){
+    //     //     cout<< " ";
+    //     // }cout<< "*";
+    //     // cout<<endl;
+    // }
 
     return 0;
 }
-
---
+++ day_3/tempCodeRunnerFile.cpp
@@ -0,0 +1,13 @@
+for (int space = 1; space <= 4; space++)
+    {
+        cout << "-";
+    }
+
+    for (int star = 1; star <= 1; star++)
+    {
+        cout << "*";
+    }
+    for (int space1 = 1; space1 <= 4; space1++)
+    {
+        cout << "-";
+    }

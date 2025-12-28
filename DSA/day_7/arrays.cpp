fe027efec7367a23f2d305924e535027c6276efc
akamalferojshaikh <akamalferojshaikh@gmail.com>
2025-12-28 21:24:03 +0530

    main

--- DSA/day_7/arrays.cpp
+++ DSA/day_7/arrays.cpp
@@ -23,13 +23,22 @@ int main(){
     // }
 
     // Q - Find min element in an array
+    // int arr[7] = {1,7,2,3,4,5,10};
+    // int min = arr[0];
+    // for(int i=1;i<7;i++){
+    //     if(min>arr[i]){
+    //         min = arr[i];
+    //     }
+    // }
+    // cout<<min;
+    // Q - Find max element in an array
     int arr[7] = {1,7,2,3,4,5,10};
-    int min = arr[0];
+    int max = arr[0];
     for(int i=1;i<7;i++){
-        if(min>arr[i]){
-            min = arr[i];
+        if(max<arr[i]){
+            max = arr[i];
         }
     }
-    cout<<min;
+    cout<<max;
     return 0;
 }
